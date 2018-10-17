e539f9a0b311c3460d2b5c73097c2fa363e309eb
Margarita R <rita.romenkova@gmail.com>
2018-10-17 19:23:29 +0300

    Adding top header black block

---
+++ src/components/main-page/header/header-contacts.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const HeaderContacts = ({ contacts }) => {
+    return (
+        <div>
+            <span className='header-top__contacts-item'>
+                <i className='hillter-icon-location'></i>
+                <a href="#">{contacts.address}</a>
+            </span>
+            <span className='header-top__contacts-item'>
+                <i className='hillter-icon-phone'></i>
+                <a href={ 'tel:' + contacts.phone }>{contacts.phone}</a>
+            </span>
+            <span className='header-top__contacts-item'>
+                <i className='hillter-icon-phone'></i>
+                <a href={ 'tel:' + contacts.sales_phone }>Отдел продаж: {contacts.sales_phone}</a>
+            </span>
+        </div>
+    );
+}
+
+export default HeaderContacts;
---
+++ src/components/main-page/header/header.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Col, Row } from 'react-bootstrap'; 
+
+import { getContacts } from './../../../store/actions/actions.js';
+import HeaderContacts from './header-contacts';
+
+import './header.css';
+
+class Header extends Component {
+
+    componentWillMount() {
+        this.props.loadData();
+    }
+
+    render() {
+        return (
+            <header>
+                <div className="header__top">
+                    <div className='container'>
+                        <Row>
+                            <Col xs={6} >
+                               <HeaderContacts contacts={this.props.ru.contacts} />
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+            </header>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        ru: {
+            contacts: state.ru.contacts
+        }
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        loadData: () => dispatch(getContacts())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
