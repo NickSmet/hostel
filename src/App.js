6e52ef2f4632d1603f04587ac3513aa15cec0954
Margarita R <rita.romenkova@gmail.com>
2018-10-17 19:22:22 +0300

    Adding new fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import './fonts/font-hilltericon.css';
+import './fonts/font-awesome.min.css';
+import './fonts/stylesheet.css';
 import './App.css';
+import Header from './components/main-page/header/header';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header></Header>
       </div>
     );
   }
