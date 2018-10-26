0be62feb484f39b6d84e2c31f3e53fd39b44bf81
Margarita R <rita.romenkova@gmail.com>
2018-10-26 03:40:45 +0300

    Addind booking block

---
+++ src/components/main-page/wubook/book.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './book.css';
+
+const Book = () => {
+    return (
+        <div className='book'>
+         <div className="left" id="_bn_widget_">
+            <a href="http://bnovo.ru/" id="_bnovo_link_" target="_blank">Bnovo</a>
+            </div>
+        </div>
+    );
+}
+
+export default Book;
