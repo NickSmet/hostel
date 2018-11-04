2bcbc24a3b4654b93b88a1231a9ba0c5cec3551c
Margarita R <rita.romenkova@gmail.com>
2018-11-04 18:02:22 +0300

    Fixes

--- src/components/contacts/contacts-contacts.js
+++ src/components/contacts/contacts-contacts.js
@@ -5,10 +5,18 @@ const Contacts = ({ contacts }) => {
         <div className='contacts-contacts'>
             <h2>Контактные данные</h2>
             <div><strong>Адрес: </strong>{ contacts.address }</div>
-            <div><strong>Ресепшн хостела: </strong>{ contacts.phone }</div>
-            <div><strong>Ресепшн гостиницы: </strong>{ contacts.phone }</div>
-            <div><strong>Отдел продаж: </strong>{ contacts.sales_phone }</div>
-            <div><strong>E-mail: </strong>{ contacts.email }</div>
+            <div><strong>Ресепшн хостела: </strong>
+                <a href={"tel:"+ contacts.phone}>{ contacts.phone }</a>
+            </div>
+            <div><strong>Ресепшн гостиницы: </strong>
+                <a href={"tel:"+ contacts.phone}>{ contacts.phone }</a>
+            </div>
+            <div><strong>Отдел продаж: </strong>
+                <a href={"tel:"+ contacts.phone}>{ contacts.sales_phone }</a>
+            </div>
+            <div><strong>E-mail: </strong>
+                <a href={"mailto:"+ contacts.email}>{ contacts.email }</a>
+            </div>
         </div>
     );
 }
