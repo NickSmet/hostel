da70ef6cc6b76dcafffe07c32c84224d539c6a08
Margarita R <rita.romenkova@gmail.com>
2018-11-04 17:40:35 +0300

    Close side menu on select item

--- src/components/main-page/header/header-side-menu.js
+++ src/components/main-page/header/header-side-menu.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import * as types from '../../../store/actions/actions';
+
 class HeaderSideMenu extends React.Component {
     render() {
         return (
             <div className={this.props.leftMenuIsOpen ? 'side-menu-items-wrapper show-side-menu' : 'side-menu-items-wrapper hide-side-menu'}>
                 <ul className='side-menu-items'>
-                    <li>
+                    <li onClick={ this.props.closeMenu }>
                         <a href='/#/'>ГЛАВНАЯ</a>
                     </li>
-                    <li>
+                    <li onClick={ this.props.closeMenu }>
                         <a href='/#/hotel'>ОТЕЛЬ</a>
                     </li>
-                    <li>
+                    <li onClick={ this.props.closeMenu }>
                         <a href='/#/hostel'>ХОСТЕЛ</a>
                     </li>
-                    <li>
+                    <li onClick={ this.props.closeMenu }>
                         <a href='/#/groups'>ГРУППОВОЕ БРОНИРОВАНИЕ</a>
                     </li>
-                    <li>
+                    <li onClick={ this.props.closeMenu }>
                         <a href='/#/visa'>ВИЗОВАЯ ПОДДЕРЖКА</a>
                     </li>
-                    <li>
+                    <li onClick={ this.props.closeMenu }>
                         <a href='/#/transfer'>ТРАНСФЕР</a>
                     </li>
-                    <li>
+                    <li onClick={ this.props.closeMenu }>
                         <a href='/#/contacts'>КОНТАКТЫ</a>
                     </li>
                 </ul>
@@ -39,4 +41,11 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(HeaderSideMenu);
+const mapDispatchToProps = dispatch => {
+    return {
+        closeMenu: () => dispatch({ type: types.OPEN_LEFT_MENU })
+    };
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(HeaderSideMenu);
